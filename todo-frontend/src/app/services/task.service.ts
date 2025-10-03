534074e41f8dbc9edef8ade3545ae4255a86ce44
bhashana99 <bhashanachamodya99@gmail.com>
2025-10-03 13:32:24 +0530

    Create taskCreate request api

---
+++ todo-frontend/src/app/services/task.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private taskCreatingUrl = 'http://localhost:8080/tasks';
+
+
+  constructor(private http: HttpClient) { }
+
+  createTask(task: Task): Observable<Task>{
+    return this.http.post<Task>(this.taskCreatingUrl, task);
+  }
+}
