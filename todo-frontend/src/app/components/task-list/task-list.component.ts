2966cea066e089fd985b1c26bd60ce46d15bd83b
bhashana99 <bhashanachamodya99@gmail.com>
2025-10-03 10:45:01 +0530

    Design itrm template and add it to list component

--- todo-frontend/src/app/components/task-list/task-list.component.ts
+++ todo-frontend/src/app/components/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskItemComponent } from '../task-item/task-item.component';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [TaskItemComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
