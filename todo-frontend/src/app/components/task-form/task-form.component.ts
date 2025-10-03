6065d69eef142840d91e4a01ed8fca6a6b82cfa0
bhashana99 <bhashanachamodya99@gmail.com>
2025-10-03 15:36:53 +0530

    adding reactive form

--- todo-frontend/src/app/components/task-form/task-form.component.ts
+++ todo-frontend/src/app/components/task-form/task-form.component.ts
@@ -1,12 +1,44 @@
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+import { Task } from './../../models/task';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss'
 })
 export class TaskFormComponent {
+
+  taskForm: FormGroup;
+  message = '';
+
+  constructor(private fb: FormBuilder,private taskService: TaskService){
+     this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+     description: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.taskForm.invalid) {
+      this.message = 'Please fill the required fields';
+      return;
+    }
+
+    const newTask: Task = this.taskForm.value;
+    this.taskService.createTask(newTask).subscribe({
+      next: () => {
+        this.message = 'Task created successfully';
+        this.taskForm.reset();
+      },
+      error: () => {
+        this.message = 'Failed to create task';
+      }
+    });
+  }
 
 }
