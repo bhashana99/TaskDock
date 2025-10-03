762d81a6b9b59380459ff714bf2b382019a4b44e
bhashana99 <bhashanachamodya99@gmail.com>
2025-10-03 11:56:22 +0530

    Create Task Response dto

---
+++ todo-backend/src/main/java/com/example/todo_app/dto/TaskResponse.java
@@ -0,0 +1,49 @@
+package com.example.todo_app.dto;
+
+import java.time.LocalDateTime;
+
+public class TaskResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+
+    public TaskResponse(Long id, String title, String description, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
