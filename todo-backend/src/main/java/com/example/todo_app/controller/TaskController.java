95cab5beee6e4e80349a5ef4a0a2da33cb92d3e4
bhashana99 <bhashanachamodya99@gmail.com>
2025-10-03 15:39:32 +0530

    Allow cross origin port 4200

--- todo-backend/src/main/java/com/example/todo_app/controller/TaskController.java
+++ todo-backend/src/main/java/com/example/todo_app/controller/TaskController.java
@@ -7,6 +7,7 @@ import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
