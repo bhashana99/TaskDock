af370b4170f20d5d18162ffb937d27aff422da69
bhashana99 <bhashanachamodya99@gmail.com>
2025-10-03 12:44:02 +0530

    Create Todo service and test todo create api using postman http://localhost:8080/tasks/createTask

---
+++ todo-backend/src/main/java/com/example/todo_app/service/TaskService.java
@@ -0,0 +1,27 @@
+package com.example.todo_app.service;
+
+import com.example.todo_app.dto.TaskRequest;
+import com.example.todo_app.dto.TaskResponse;
+import com.example.todo_app.entity.Task;
+import com.example.todo_app.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public TaskResponse createTask(TaskRequest request){
+        Task task = new Task();
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+
+        Task saved = repository.save(task);
+
+        return new TaskResponse(saved.getId(), saved.getTitle(), saved.getDescription(), saved.getCreatedAt());
+    }
+}
